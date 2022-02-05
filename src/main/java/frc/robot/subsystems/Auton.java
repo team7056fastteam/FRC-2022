4d7e742b6cdcedf35b5e2c9d6e53bcffa8d92de1
schristie1109 <schristie1109@yahoo.com>
2022-02-05 11:05:04 -0500

    Auton drive functions

--- src/main/java/frc/robot/subsystems/Auton.java
+++ src/main/java/frc/robot/subsystems/Auton.java
@@ -1,32 +1,37 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Robot;
 
 public class Auton {
 
     Robot robot;
+    Drivetrain _drive;
 
     double currentTime;
     char autonMode;
+
     boolean hasStarted;
+    boolean hasReachedPosition = false;
+    int phase = 0;
 
-    double navX = robot.getXPos();
-    double navY = robot.getYPos();
-    double[] position = {navX, navY};
-
-    double[] driveTo = {0.0, 0.0};
+    double currentX = robot.getXPos();
+    double currentY = robot.getYPos();
+    double currentZ = robot.getGyroscopeRotation();
 
     private final Joystick driver = new Joystick(0);
 
     public Auton() {
         robot = new Robot();
+        _drive = new Drivetrain();
         currentTime = robot.getCurrentTime();
     }
 
     public void robotInit() {
         autonMode = 'a';
         hasStarted = false;
+        phase = 1;
     }
     
     public void robotPeriodic() {
@@ -46,6 +51,7 @@ public class Auton {
     public void autonomousInit() {
         // End autonomous selector when the match begins
         hasStarted = true;
+        hasReachedPosition = false;
     }
 
     public void autonomousPeriodic() {
@@ -58,26 +64,72 @@ public class Auton {
     }
 
     public void autonA() {
-
+        if (phase == 0) {
+            driveTo(10, 10, 0);
+        }
+        else if (phase == 1) {
+            driveTo(0, 15, 90);
+        }
+        else if (phase == 2) {
+            driveTo(20, 20, 270);
+        }
     }
 
-    public void autonB() {
+    public void autonB() { }
 
+    public void autonC() { }
+
+    public void autonD() { }
+
+    public void autonE() { }
+
+    public void autonF() { }
+
+    /** Drive functions */
+    public void driveTo(double desiredX, double desiredY, double desiredZ) {
+        double xSpeed = calculateSpeed(desiredX - currentX);
+        double ySpeed = calculateSpeed(desiredY - currentY);
+        double rotation = calculateSpeed(desiredZ - currentZ);
+
+        _drive.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+            xSpeed * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
+            ySpeed * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
+            rotation * Drivetrain.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+            _drive.getRotation()
+        ));
+
+        // End function usage if we are moving to the next autonomous phase
+        if (xSpeed == 0 && ySpeed == 0 && rotation == 0) { phase++; }
     }
 
-    public void autonC() {
+    public double calculateSpeed(double distance) {
+        // Do not change these values unless you know what you are doing!
+        double max = 0.7;
+        double rate = 0.008;
+        double fixedDistance = Math.abs(distance);
+        int direction = 1;
 
+        double speed = Math.pow(2 * fixedDistance, 2) * rate;
+        if (speed > max) { speed = max - 0.01; }
+        if (speed < 0) { speed = 0.0; }
+        if (fixedDistance < 0.01) { speed = 0.0; }
+
+        if (distance < 0) { direction = -1; }
+
+        return direction * speed;
     }
 
-    public void autonD() {
+    public double calculateRotation(double angle) {
+        // Do not change these values unless you know what you are doing!
+        double max = 0.8;
+        double rate = 0.0005;
 
-    }
+        double speed = Math.pow(2 * angle, 2) * rate;
+        if (speed > max) { speed = max - 0.01; }
+        if (speed < 0) { speed = 0.0; }
+        if (angle < 0.01) { speed = 0.0; }
 
-    public void autonE() {
 
-    }
-
-    public void autonF() {
-
+        return speed;
     }
 }
